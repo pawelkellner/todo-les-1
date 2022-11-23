5666fd32069cab36455c40a7d635e87f96a9ae79
Pawel <smrpawel@gmail.com>
2022-11-23 15:27:02 +0100

    finished lesson

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,22 +2,27 @@ import { useState, useEffect } from "react"
 import "./Item.css";
 
 const Item = (props) => {
-    let classNames = ""
-
     const [done, setDone] = useState(false);
+    const [className, setClassname ] = useState("todo__item")
 
-    if(done){
-        classNames = "todo__item todo__item--done";
-    }else{
-        classNames = "todo__item";
-    }
+    useEffect(() =>{
+        setDone(props.done);
+    }, [])
+
+    useEffect(() =>{
+        if(done){
+            setClassname("todo__item todo__item--done")
+        }else{
+            setClassname("todo__item")
+        }
+    }, [done]);
 
     const onItemClick = () => {
         setDone(!done);
     }
 
     return (
-        <li onClick={onItemClick} className={classNames}>{props.name} </li>
+        <li onClick={onItemClick} className={className}>{props.name} </li>
     )
 
 }
